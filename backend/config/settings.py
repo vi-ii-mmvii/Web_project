c6d57b0f7ee2409711ddde7eaaa52923d389ed5d
Deniyel <daniel_bedelov@mail.ru>
2026-04-22 14:12:38 +0500

    modify backend settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f3t2-_8#y75^!gv1+b4g_^yhowvju9$^uxw#*$$@f@+465meat
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lavish-passion-projext.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
